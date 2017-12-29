a6107f8aaf40aa52ed1c7e60c0250ef254144161
raryosu <me@raryosu.info>
2017-12-29 21:32:55 +0900

    Part iii 途中まで

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import com.sun.org.apache.xpath.internal.operations.Bool
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,17 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    operator fun contains(item: MyDate): Boolean = start <= item && item <= end
+    override fun iterator(): Iterator<MyDate> = DataIterator(this)
+}
+
+class DataIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean = current <= dateRange.end
+}
